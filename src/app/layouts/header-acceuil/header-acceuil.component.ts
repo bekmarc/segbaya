dbc1a6788cd66bb03892191db51bed58067666c6
bekmarc <104980306+bekmarc@users.noreply.github.com>
2022-10-19 14:53:00 +0000

    header && architecture

---
+++ src/app/layouts/header-acceuil/header-acceuil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-acceuil',
+  templateUrl: './header-acceuil.component.html',
+  styleUrls: ['./header-acceuil.component.scss']
+})
+export class HeaderAcceuilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/signing/signing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signing',
+  templateUrl: './signing.component.html',
+  styleUrls: ['./signing.component.scss']
+})
+export class SigningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
